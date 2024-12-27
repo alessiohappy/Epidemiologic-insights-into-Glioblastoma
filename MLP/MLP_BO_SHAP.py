cb861b2108a183d3af0c33d158f4664521def288
Alessio Felici <alessiofelici.do@gmail.com>
2024-12-27 12:00:43 +0100

    Update and rename MLP_BO_SHAP.py to MLP/MLP_BO_SHAP.py

--- MLP/MLP_BO_SHAP.py
+++ MLP/MLP_BO_SHAP.py
@@ -40,16 +40,11 @@ y = df_no_ID['status']
 
 df_no_Pheno = df_no_ID.drop('status', axis = 1)
 
-cont_var = ["age", "standing_height", "right_hand_grip_strength",
-            "number_in_household", "cystatin_c",
-            "glycated_haemoglobin_HbA1c", "IGF1",
-            "systolic_blood_pressure_AutomatedReading", "weighted"]
+cont_var = [] #continuous features
     
-cat_var = ["qualifications", "ownORrent_accomodation_lived",
-           "leisure_social_activities", "current_employment_status",
-           "weekly_usage_of_mobile_phone_in_last_3_months"]
+cat_var = [] #categorical features
 
-other_var = ['sex']
+other_var = [] #binary features
     
 df_cat_var = df_no_Pheno.drop(cont_var, axis = 1)
 df_cat_var = df_cat_var.drop(other_var, axis = 1)
@@ -69,36 +64,17 @@ cat_var_name = df_cat_var.columns
 new_cat_var_names = enc.get_feature_names_out(cat_var_name)
 new_df_cat_var = pd.DataFrame(cat_var_encoded, columns = new_cat_var_names)
 
+#preprocessing: normalization with Z-score
+scaler =  StandardScaler()
+scaler.fit(df_cont_var) 
+cont_var_scaled = scaler.transform(df_cont_var)
+
+new_df_cont_var = pd.DataFrame(cont_var_scaled, columns = cont_var)
 
 # Create a df with normalize variables and cat variables 
-new_df = np.concatenate((df_cont_var, df_other_var), axis=1)
+new_df = np.concatenate((new_df_cont_var, df_other_var), axis=1)
 new_df_1 = np.concatenate((new_df, new_df_cat_var), axis=1)
-new_col_names = ["age", "standing_height", "right_hand_grip_strength",
-               "number_in_household", "cystatin_c",
-               "glycated_haemoglobin_HbA1c", "IGF1",
-               "systolic_blood_pressure_AutomatedReading", "weighted", "sex",
-               "qualifications_0.0", "qualifications_1.0", "qualifications_2.0",
-               "qualifications_3.0", "qualifications_4.0", "qualifications_5.0", 
-               "qualifications_6.0", "ownORrent_accomodation_lived_1.0", 
-               "ownORrent_accomodation_lived_2.0",
-               "ownORrent_accomodation_lived_3.0", 
-               "ownORrent_accomodation_lived_4.0", 
-               "ownORrent_accomodation_lived_5.0", 
-               "ownORrent_accomodation_lived_6.0", 
-               "weekly_usage_of_mobile_phone_in_last_3_months_-1.0", 
-               "weekly_usage_of_mobile_phone_in_last_3_months_0.0", 
-               "weekly_usage_of_mobile_phone_in_last_3_months_1.0", 
-               "weekly_usage_of_mobile_phone_in_last_3_months_2.0", 
-               "weekly_usage_of_mobile_phone_in_last_3_months_3.0", 
-               "weekly_usage_of_mobile_phone_in_last_3_months_4.0", 
-               "weekly_usage_of_mobile_phone_in_last_3_months_5.0", 
-               "leisure_social_activities_0.0", "leisure_social_activities_1.0", 
-               "leisure_social_activities_2.0", "leisure_social_activities_3.0", 
-               "leisure_social_activities_4.0", "leisure_social_activities_5.0",
-               "current_employment_status_1.0", "current_employment_status_2.0",
-               "current_employment_status_3.0", "current_employment_status_4.0",
-               "current_employment_status_5.0", "current_employment_status_6.0",
-               "current_employment_status_7.0", "current_employment_status_8.0"]
+new_col_names = [] #name of continuous, binary and categorical (for each category) features
 
 X = pd.DataFrame(data=new_df_1, columns=new_col_names)
 
@@ -206,21 +182,8 @@ plt.show()
 
 '''#4: SHapely Additive exPlanations'''
 # Creazione del modello SHAP Explainer
-#final_shap_df_v2 = pd.DataFrame(data = X_res, columns = new_col_names)
-#shap_values_tree = shap.KernelExplainer(svc.predict, data = X_train).shap_values(X = final_shap_df_v2, y = y_res)
-
-final_shap_df_v2 = pd.read_csv("C:\\Users\\R2D2\\OneDrive - University of Pisa\\"
-                               + "UniPi\\OneDrive - University of Pisa\\UniPi\\"
-                               + "Articoli\\Glioblastoma\\MLP_articolo_gbm\\"
-                               + "final_shap_df_v2_mlp.csv", sep = ",")
-
-final_shap_df_v2 = final_shap_df_v2.drop("Unnamed: 0", axis = 1)
-
-shap_values_tree = np.load("C:\\Users\\R2D2\\OneDrive - University of Pisa\\"
-                           + "UniPi\\OneDrive - University of Pisa\\UniPi\\"
-                           + "Articoli\\Glioblastoma\\MLP_articolo_gbm\\"
-                           + "shap_values_tree_mlp.npy")
-
+final_shap_df_v2 = pd.DataFrame(data = X_res, columns = new_col_names)
+shap_values_tree = shap.KernelExplainer(svc.predict, data = X_train).shap_values(X = final_shap_df_v2, y = y_res)
 print(shap_values_tree)
 
 shap_values_tree_cases = shap_values_tree
@@ -252,10 +215,7 @@ for values in shap_values_categorical.values:
 # Replace shap values for categorical variables
 new_shap_values_categorical = np.array(new_shap_values)
 
-categorical_variables_name = ["qualifications", "ownORrent_accomodation_lived",
-                              "current_employment_status",
-                              "weekly_usage_of_mobile_phone_in_last_3_months",
-                              "leisure_social_activities", "Null"]
+categorical_variables_name = [] # names of categorical variables + Null
 
 new_shap_values_categorical_name = pd.DataFrame(new_shap_values_categorical, columns = categorical_variables_name)
 
@@ -265,15 +225,7 @@ categorical_shap_values_name = new_shap_values_categorical_name.drop('Null', axi
 continue_shap_value = shap_values_tree_name.drop(feature_names, axis = 1)
 
 Final_shap_value = np.concatenate((continue_shap_value, categorical_shap_values_name), axis = 1)
-columns_for_final_shap_value = ["Age", "Standing height", "Right hand grip strength",
-                                "Number in household", "Cystatin C",
-                                "Glycated haemoglobin (HbA1c)", "IGF1",
-                                "Systolic blood pressure (Automated reading)",
-                                "Weighted PGS", "Sex", "Qualifications",
-                                "Own or rent accommodation lived",
-                                "Current employment status",
-                                "Weekly usage of mobile phone in last 3 months",
-                                "Leisure/social activities"]
+columns_for_final_shap_value = [] # name of all columns
 
 Final_shap_values_name = pd.DataFrame(Final_shap_value, columns = columns_for_final_shap_value)
 
